100167e6c57613da5ca999fc31a688b7da7db4dd
Matheus Barcellos <mathbar4@hotmail.com>
2021-12-13 21:54:14 -0600

    Finished work

--- comp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-g++ -o main main.cpp ./drivers/AddressBookType.cpp ./drivers/AddressType.cpp ./drivers/DateType.cpp ./drivers/ExtDateType.cpp ./drivers/Person.cpp ./drivers/ExtPerson.cpp
